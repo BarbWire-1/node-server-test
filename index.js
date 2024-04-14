513a468fcd733307cc8298aba09aca6c8bd6d1b7
Barbara Kälin <67426447+BarbWire-1@users.noreply.github.com>
2024-04-14 17:16:43 +0200

    changed the rinternal request

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const server = require('./server/myServer');
 // routes actually like flattened path, arrays not included, not tested for deeply nested - might need flattening the JSON directly when in (?)
 // usage of server.internal, an instance of InternalRequestHandler
 
-//TODO there is something really badly wrong! Internal should only fake the request not handle response in any way
+//TODO there is something really badly wrong! Internal should only fake the request not handle response in any way - so sometimes needs to run rs several times to get the data...that is :(
 const requestParams = {
 	method: 'GET',
 	path: '/api/products/price/89.99',
@@ -20,10 +20,16 @@ server.internal
 	.makeRequest(requestParams) // I FIRST need to have the response passed the server's handleRequest I think...
 	.then((responseData) => {
 		let result = JSON.parse(responseData);
-		console.log('Got Data from internal request!', result);
+		let message = '';
 
-		if (Array.isArray(result))
+		if (result.length) {
+			message = `Got Data from internal request!`;
 			result.forEach((r) => console.log('id: ', r.id));
+		} else {
+			message =
+				"That either didn't find a match, or more serious didn't WORK!";
+		}
+		console.log(message, result);
 	})
 	.catch((error) => {
 		console.error(error);
