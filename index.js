72edb589e71ad2f5866f58013c7709fea9663b09
Barbara Kälin <67426447+BarbWire-1@users.noreply.github.com>
2024-04-14 13:52:16 +0200

    comment on possible routes (how they get generated or read)

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const server = require('./server/myServer');
 // there must be an issue in timing
 
 
-//api/products/ or '/api/proDucts/[key]/[value]
+//api/products/ or '/api/products/[key]/[value]
 // routes actually like flattened path, arrays not included, not tested for deeply nested - might need flattening the JSON directly when in (?)
 // usage of server.internal, an instance of InternalRequestHandler
 const requestParams = {
