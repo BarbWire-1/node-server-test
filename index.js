2a851f85390c7dfa63ba9d9802cf75d63ce80171
Barbara Kälin <67426447+BarbWire-1@users.noreply.github.com>
2024-04-14 17:40:34 +0200

    reading server._response directly in internal

--- index.js
+++ index.js
@@ -18,19 +18,19 @@ const requestParams = {
 
 server.internal
 	.makeRequest(requestParams) // I FIRST need to have the response passed the server's handleRequest I think...
-	.then((responseData) => {
-		let result = JSON.parse(responseData);
+	.then(() => {
+		let result = server._response;// just for testing using the initial private prop
 		let message = '';
 
-		if (result.length) {
-			message = `Got Data from internal request!`;
+        if (result.length) {
+            message = "Yeah! I made it through the async hell!"
 			result.forEach((r) => console.log('id: ', r.id));
 		} else {
 			message =
                 "That either didn't find a match, or more serious didn't WORK!";
             // also logs message: 'No Match Found"} from controller.getAll()
 		}
-		console.log(message, result);
+		console.log(message, server._response);
 	})
 	.catch((error) => {
 		console.error(error);
