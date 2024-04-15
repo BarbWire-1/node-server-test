ec780d7a6ddf6b4af0147ce80adf42cc7c2955f7
Barbara Kälin <67426447+BarbWire-1@users.noreply.github.com>
2024-04-15 12:27:45 +0200

    fixed internal (??)

--- index.js
+++ index.js
@@ -7,11 +7,11 @@ const server = require('./server/myServer');
 // routes actually like flattened path, arrays not included, not tested for deeply nested - might need flattening the JSON directly when in (?)
 // usage of server.internal, an instance of InternalRequestHandler
 
-//TODO there is something really badly wrong! Internal should only fake the request not handle response in any way - so sometimes needs to run rs several times to get the data...that is :(
+//TODO there is something really badly wrong! Internal should only fake the request not handle response in any way - so sometimes needs to run rs several times to get the data and does multiple requests...that is :(
 const requestParams = {
 	method: 'GET',
 	path: '/api/products/price/89.99',
-	//data: null, // only if required - on POST or PUT (what about PATCH in general?)
+	//data: {"price": 89.99}, // only if required - on POST or PUT (what about PATCH in general?)
 	// pass these two from the consuming server-instance??
 	//hostname: server.host,
 	//port: server.port,
@@ -23,14 +23,16 @@ server.internal
 		let result = server._response;// just for testing using the initial private prop
 		let message = '';
 
-        if (result.length) {
-            message = "Yeah! I made it through the async (params, variableNames...???) hell!"
-			result.forEach((r) => console.log('id: ', r.id));
-		} else {
-			message =
-                "That either didn't find a match, or more serious didn't WORK!";
-            // also logs message: 'No Match Found"} from controller.getAll()
-		}
+
+            if (result?.length) {
+                message = "Yeah! I made it through the async (params, variableNames...???) hell!"
+                result.forEach((r) => console.log('id: ', r.id));
+            } else {
+                message =
+                    "That either didn't find a match, or more serious didn't WORK!";
+                // also logs message: 'No Match Found"} from controller.getAll()
+            }
+
 		console.log(message, server._response);
 	})
 	.catch((error) => {
--- serverLib/jsonClasses/InternalRequestHandler.js
+++ serverLib/jsonClasses/InternalRequestHandler.js
@@ -1,68 +1,79 @@
-/* The `InternalRequestHandler` class in JavaScript is designed to make internal HTTP requests to a
-specified host and port, handling response data and passing it to a server's `handleRequest` method. */
+
 const http = require('http');
-// This is in a way NONSENSE- the request should only be PASSED!!!!
+// making a request and passing req,res to server.handleRequest
 class InternalRequestHandler {
 	constructor(server) {
 		this.server = server;
 	}
 
 	async makeRequest(requestOptions) {
-		return new Promise((resolve, reject) => {
-			const { path, method, data } = requestOptions;
+		const { path, method, data } = requestOptions;
+		let requestDataJSON = null;
 
-			// Define request details
-			const options = {
-				hostname: this.server.host,
-				port: this.server.port,
-				...requestOptions,
-				headers: {
-					'Content-Type': 'application/json', // Assuming JSON data
-				},
+		// If data is provided, parse it as JSON
+		if (data) {
+			try {
+				requestDataJSON = JSON.parse(data);
+			} catch (error) {
+				console.error('Error parsing request data as JSON:', error);
+				throw error;
+			}
+		}
+
+		// Define request details
+		const options = {
+			hostname: this.server.host,
+			port: this.server.port,
+			...requestOptions,
+			headers: {
+				'Content-Type': 'application/json',
+			},
+		};
+
+		try {
+			// Make the HTTP request
+			const responseData = await new Promise((resolve, reject) => {
+				const req = http.request(options, (res) => {
+					let responseData = '';
+
+					// Collect response data
+					res.on('data', (chunk) => {
+						responseData += chunk;
+					});
+					res.on('end', () => {
+						resolve(responseData);
+					});
+				});
+
+				req.on('error', (error) => {
+					reject(error);
+				});
+
+				// Write request data to the request body if it exists
+				if (requestDataJSON) {
+					req.write(JSON.stringify(requestDataJSON));
+				}
+
+				req.end();
+			});
+
+			// Pass the request and response data to the server's handleRequest method
+			const fakeResponse = {
+				// Create a mock response object with minimal functionality
+				writeHead: () => {},
+				end: () => {},
 			};
-			//console.log(options)
+			await this.server.handleRequest(
+				{ url: path, method, data: requestDataJSON },
+				fakeResponse
+			);
 
-			// Create the request ->
-			//TODO implement getResponse here
-			const req = http.request(options, (res) => {
-				let responseData = '';
-
-				// Collect response data
-				res.on('data', (chunk) => {
-					responseData += chunk;
-				});
-				res.on('end', () => {
-					resolve(responseData);
-				});
-			});
-
-			req.on('error', (error) => {
-				reject(error);
-			});
-
-			if (data) req.write(JSON.stringify(data));
-
-			req.end();
-		});
+			return responseData;
+		} catch (error) {
+			console.error('Internal request error:', error);
+			throw error;
+		}
 	}
-
-	// async makeInternalRequest(requestOptions) {
-	// 	try {
-	// 		// Make the internal request
-	// 		const responseData = await this.makeRequest(requestOptions);
-	// 		// Pass the response data to the server's handleRequest method
-	// 		const fakeResponse = {
-	// 			writeHead: () => {},
-	// 			end: () => {},
-	// 		}; // Create a mock response object
-	// 		await this.server.handleRequest(
-	// 			{ url: requestOptions.path, method: requestOptions.method },
-	// 			fakeResponse
-	// 		);
-	// 	} catch (error) {
-	// 		console.error('Internal request error:', error);
-	// 	}
-	// }
 }
 
 module.exports = InternalRequestHandler;
